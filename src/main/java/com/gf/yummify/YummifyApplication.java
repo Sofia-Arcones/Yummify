95dc44b963988e93fe27155995f7ad15895f1a79
Sofia-Arcones <sarcones@serbatic.es>
2024-10-14 13:03:03 +0200

    Initial commit

---
+++ src/main/java/com/gf/yummify/YummifyApplication.java
@@ -0,0 +1,13 @@
+package com.gf.yummify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gf/yummify/YummifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gf.yummify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
