ee57cf197eb165f8624bc50698e24d71c8ce0881
Sofia-Arcones <sarcones@serbatic.es>
2024-11-12 11:59:26 +0100

    Cambios en el servicio de recetas

--- src/main/java/com/gf/yummify/business/services/RecipeService.java
+++ src/main/java/com/gf/yummify/business/services/RecipeService.java
@@ -2,6 +2,7 @@ package com.gf.yummify.business.services;
 
 import com.gf.yummify.data.entity.Recipe;
 import com.gf.yummify.presentation.dto.RecipeRequestDTO;
+import com.gf.yummify.presentation.dto.RecipeResponseDTO;
 import org.springframework.security.core.Authentication;
 
 import java.util.UUID;
@@ -12,4 +13,5 @@ public interface RecipeService {
     Recipe findRecipeById(UUID id);
 
     void deleteRecipe(UUID id);
+    RecipeResponseDTO getRecipeResponseDTO(UUID id);
 }
