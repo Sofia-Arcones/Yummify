13cd9e7b63910254d5e21f83a07823063d95b2d8
Sofia-Arcones <sarcones@serbatic.es>
2024-11-26 19:42:19 +0100

    Arreglado filtrado y busqueda

--- src/main/java/com/gf/yummify/presentation/controller/HomeController.java
+++ src/main/java/com/gf/yummify/presentation/controller/HomeController.java
@@ -25,7 +25,6 @@ public class HomeController {
         this.ingredientService = ingredientService;
     }
 
-    //TODO que no se cargue dos veces el html
     @GetMapping("/home")
     public String home(Model model,
                        @RequestParam(value = "page", defaultValue = "0") int page,
@@ -36,7 +35,7 @@ public class HomeController {
                        @RequestParam(value = "ingredients", required = false) List<String> ingredients,
                        @RequestParam(value = "ingredientType", required = false) IngredientType ingredientType,
                        @RequestParam(value = "search", required = false) String search,
-                       @RequestParam(value = "isAjax", defaultValue = "false") boolean isAjax) {  // Cambiado aquí
+                       @RequestParam(value = "isAjax", defaultValue = "false") boolean isAjax) {
         try {
             System.out.println("Search term: " + search);
             model.addAttribute("ingredientTypes", IngredientType.values());
@@ -49,7 +48,7 @@ public class HomeController {
                 model.addAttribute("recipes", recipeService.findFilteredRecipes(page, size, difficulty, portions, tags, ingredients, ingredientType));
             }
 
-            if (isAjax) {  // Cambiado aquí
+            if (isAjax) {
                 return "fragments/recipeContainer :: recipesContainer";
             }
         } catch (Exception ex) {
@@ -57,45 +56,4 @@ public class HomeController {
         }
         return "index";
     }
-
-//    @GetMapping("/home")
-//    public String home(Model model,
-//                       @RequestParam(value = "page", defaultValue = "0") int page,
-//                       @RequestParam(value = "size", defaultValue = "3") int size,
-//                       @RequestParam(value = "difficulty", required = false) Difficulty difficulty,
-//                       @RequestParam(value = "portions", required = false) Integer portions,
-//                       @RequestParam(value = "tags", required = false) List<String> tags,
-//                       @RequestParam(value = "ingredients", required = false) List<String> ingredients,
-//                       @RequestParam(value = "ingredientType", required = false) IngredientType ingredientType,
-//                       @RequestParam(value = "search", required = false) String search,
-//                       @RequestParam(value = "isAjax", defaultValue = "false") boolean isAjax) {
-//        try {
-//            // Verificar si todos los parámetros son nulos o vacíos
-//            boolean isEmptySearchParams = (search == null || search.isEmpty()) &&
-//                    (difficulty == null) &&
-//                    (portions == null) &&
-//                    (tags == null || tags.isEmpty()) &&
-//                    (ingredients == null || ingredients.isEmpty()) &&
-//                    (ingredientType == null);
-//            model.addAttribute("ingredientTypes", IngredientType.values());
-//            model.addAttribute("difficulties", Difficulty.values());
-//
-//            if (search != null && !search.isEmpty()) {
-//                model.addAttribute("searchTerm", search);
-//                model.addAttribute("recipes", recipeService.searchRecipes(search, page, size));
-//            } else {
-//                model.addAttribute("recipes", recipeService.findFilteredRecipes(page, size, difficulty, portions, tags, ingredients, ingredientType));
-//            }
-//
-//            // Si todos los parámetros son nulos, retorna "index", si no, retorna "fragments/recipeContainer :: recipesContainer"
-//            if (isEmptySearchParams) {
-//                return "index";
-//            }
-//            return "fragments/recipeContainer :: recipesContainer";
-//        } catch (Exception ex) {
-//            model.addAttribute("error", ex.getMessage());
-//        }
-//        return "index";
-//    }
-
 }
