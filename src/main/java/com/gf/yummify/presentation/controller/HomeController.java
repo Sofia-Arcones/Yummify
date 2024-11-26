20f28328b0d79192f327fc2bde5e6c093c340e87
Sofia-Arcones <sarcones@serbatic.es>
2024-11-26 14:03:50 +0100

    Filtros en vista

--- src/main/java/com/gf/yummify/presentation/controller/HomeController.java
+++ src/main/java/com/gf/yummify/presentation/controller/HomeController.java
@@ -1,18 +1,29 @@
 package com.gf.yummify.presentation.controller;
 
+import com.gf.yummify.business.services.IngredientService;
 import com.gf.yummify.business.services.RecipeService;
+import com.gf.yummify.business.services.TagService;
+import com.gf.yummify.data.entity.Tag;
+import com.gf.yummify.data.enums.Difficulty;
+import com.gf.yummify.data.enums.IngredientType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     private RecipeService recipeService;
+    private TagService tagService;
+    private IngredientService ingredientService;
 
-    public HomeController(RecipeService recipeService) {
+    public HomeController(RecipeService recipeService, TagService tagService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.tagService = tagService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/home")
@@ -20,6 +31,8 @@ public class HomeController {
                        @RequestParam(value = "page", defaultValue = "0") int page,
                        @RequestParam(value = "size", defaultValue = "3") int size) {
         try {
+            model.addAttribute("ingredientTypes", IngredientType.values());
+            model.addAttribute("difficulties", Difficulty.values());
             model.addAttribute("recipes", recipeService.findAllRecipes(page, size));
         } catch (Exception ex) {
             model.addAttribute("error", ex.getMessage());
